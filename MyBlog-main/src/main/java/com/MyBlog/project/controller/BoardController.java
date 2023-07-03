c5f24f731f0ac79797bb051ff8685dc335e32594
kimlaura <laurakimyejin>
2023-07-03 14:31:38 +0900

    Initial commit

---
+++ MyBlog-main/src/main/java/com/MyBlog/project/controller/BoardController.java
@@ -0,0 +1,76 @@
+package com.MyBlog.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.MyBlog.project.dto.BoardDto;
+import com.MyBlog.project.model.Board;
+import com.MyBlog.project.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class BoardController {
+	
+	@Autowired
+	private final BoardService boardService;
+	
+//	//전체 글 보기
+//	@GetMapping("/")
+//	public String index(Model model, @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+//		Page<Board> board = boardService.list(pageable);
+//		model.addAttribute("boards", board);
+//		return "index";
+//	}
+	
+	//== admin ==//-데이터 목록 조회
+	@GetMapping("/board/datalist")
+	public String dataList(Model model, @PageableDefault(size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+//		Page<Board> dataList = boardService.categoryList(pageable, "daily");
+		Page<Board> board = boardService.list(pageable);
+//		List<BoardDto> board = boardService.findAll();
+		model.addAttribute("boards", board); //board->daily
+		return "category/datalist";
+	}
+
+	//== admin ==//
+//	@GetMapping("/board/knowledge")
+//    public String knowledgeList(Model model, @PageableDefault(size = 5, sort = "id",
+//            direction = Sort.Direction.DESC) Pageable pageable) {
+////        Page<Board> knowledge = boardService.categoryList(pageable, "knowledge");
+//		Page<Board> board = boardService.list(pageable);
+//		model.addAttribute("boards", board);//board->knowledge
+//        return "category/knowledge";
+//    }
+	
+//	//== 게시글 등록 ==//
+//	@GetMapping("/board/writeForm")
+//	public String writeForm() {
+//		return "board/writeForm";
+//	}
+
+	//== 상세 조회, 조회수 업데이트 ==//
+	@GetMapping("/board/{id}")
+	public String detail (@PathVariable Long id, Model model) {
+		BoardDto detail = boardService.details(id);
+		model.addAttribute("board", detail);
+		return "board/detail";
+	}
+
+
+//	//== 게시글 수정 ==//
+//    @GetMapping("/board/{id}/updateForm")
+//    public String updateForm(@PathVariable Long id, Model model) {
+//        BoardDto articles = boardService.details(id);
+//        model.addAttribute("board", articles);
+//        return "board/updateForm";
+//    }
+}
